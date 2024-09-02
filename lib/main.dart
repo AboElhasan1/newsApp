df2e002fcb25cc1b0a046421d503345bb377d6d3
AboElhasan <ahmadnaser4678@gmail.com>
2024-09-02 18:07:14 +0300

    da

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/core/application_theme_manager.dart';
+import 'package:news_app/core/config/app_router.dart';
+import 'package:news_app/core/page_routes_names.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News App',
+      theme: ApplicationThemeManager.themeData,
+      initialRoute: PageRouteName.home,
+      onGenerateRoute: AppRouter.onGenerate,
+    );
+  }
+}
